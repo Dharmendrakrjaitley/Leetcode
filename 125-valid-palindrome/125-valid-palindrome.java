6ec18974cb960697052523a6d77c3effdb5d76fb
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2022-04-06 11:45:37 +0530

    Time: 831 ms (15.68%), Space: 47.5 MB (18.68%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^A-Za-z0-9]","");
+        s=s.toLowerCase();
+        
+        return checkForPalindrome(s);
+       
+    }
+    public boolean checkForPalindrome(String s)
+    {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
