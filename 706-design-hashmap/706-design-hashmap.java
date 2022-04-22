7796ba6b77dfa29cd4ebf2620b5dba0a069b8321
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2022-04-22 23:31:25 +0530

    Time: 108 ms (14.87%), Space: 110.5 MB (13.30%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,26 +1,22 @@
 class MyHashMap {
 
-    public List<Integer> ls;
+  
     public int[] arr=new int[1000001];
     public MyHashMap() {
-       // ls=new ArrayList<Integer>(10);
         Arrays.fill(arr,-1);
     }
    
     public void put(int key, int value) {
-        System.out.println("key "+ key + "value "+ value);
-        //ls.add(key,value);
+        
         arr[key]=value;
     }
     
     public int get(int key) {
-      //  int temp=(ls.get(key) >= 0) ? ls.get(key) : -1;
         int temp=(arr[key] >= 0) ? arr[key] : -1;
         return temp;
     }
     
     public void remove(int key) {
-       // ls.remove(key);
         arr[key]=-1;
     }
 }
