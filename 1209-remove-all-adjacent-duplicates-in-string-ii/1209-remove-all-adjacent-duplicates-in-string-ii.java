61f3f037bac4ad7ad5d9034086e25edbd143e713
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2022-05-06 23:11:26 +0530

    Time: 84 ms (32.33%), Space: 49.7 MB (53.52%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,48 @@
+// class Solution {
+//     public String removeDuplicates(String s, int k) {
+        
+//         Stack<HashMap<Character,Integer>> stack=new Stack<HashMap<Character,Integer>>();
+//         String str="";
+//         for(char c:s.toCharArray())
+//         {
+//             if(!stack.empty() && stack.peek().containsKey(c))
+//             {
+                
+//                 stack.push(new HashMap(c,stack.peek().get(c)+1));
+//                 if(stack.peek().get(c)==k-1)
+//                     stack.pop();
+//             }
+//             else
+//                 stack.push(new HashMap(c,1));
+//         }
+        
+//         while(!stack.empty())
+//         {
+//             Set set=stack.pop().keySet();
+//             List<Character> list = new ArrayList<Character>(set);
+//             str=list.get(0)+str;
+//         }
+//         return str;
+//     }
+// }
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<int []> Master = new Stack<>();
+        
+        for(char ch : s.toCharArray()){
+            if(!Master.isEmpty() && Master.peek()[0] == ch){
+                Master.peek()[1]++;
+            }
+            else Master.push(new int[]{ch, 1});
+            if(Master.peek()[1] == k) Master.pop();
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!Master.isEmpty()){
+            int top[] = Master.pop();
+            while(top[1] --> 0)
+                sb.append((char)top[0]);
+        }
+        return sb.reverse().toString();
+    }
+}
