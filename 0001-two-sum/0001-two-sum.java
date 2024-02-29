dbeb0878a251c8a3490e6ff7ae1ee6e5735841cc
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-02-29 16:53:14 +0530

    Time: 432 ms (5.06%), Space: 44.6 MB (77.45%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        //1st approach
+        
+        // int length=nums.length;
+        // int i=0;
+        // int j=0;
+        // boolean flag=false;
+        // for( i=0;i<length;i++)
+        // {
+        //     int temp=target-nums[i];
+        //     for( j=i+1;j<length;j++)
+        //     {
+        //         if(temp==nums[j])
+        //         {
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(flag)
+        //         break;
+        // }
+        // return new int[]{i,j};
+        
+        
+        //2nd Approach
+        
+//         Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+//         int i=0;
+//         int j=0;
+//         for(i=0;i<nums.length;i++)
+//         {
+//             int temp=target-nums[i];
+//             if(map.containsKey(temp))
+//             {
+//                j= map.get(temp);
+//                    break;
+//             }
+//              map.put(nums[i],i);
+//         }
+        
+//         return new int[]{i,j};
+        
+        
+        //Third approach
+        
+        ArrayList as=new ArrayList<Integer>();
+        for(int num: nums)
+        {
+            as.add(num);
+        }
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            int temp=target-nums[i];
+            if(as.contains(temp) && as.indexOf(temp)!=i)
+            {
+                return new int[]{i,as.indexOf(temp)};
+            }
+        }
+        return new int[]{};
+    }
+}
