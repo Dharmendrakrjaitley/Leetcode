b4a5c9a6c5ca3943a4310fb20f815073da256300
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-02-29 16:09:49 +0530

    Time: 3 ms (88.98%), Space: 44.3 MB (51.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        int l1=s.length();
+        int l2=t.length();
+        if(l1!=l2)
+            return false;
+        char[] s1=s.toCharArray();
+        Arrays.sort(s1);
+        s=new String(s1);
+        char[] t1=t.toCharArray();
+        Arrays.sort(t1);
+        t=new String(t1);
+        
+        for(int i=0;i<l1;i++)
+        {
+            // if(s.charAt(i)!=t.charAt(i))
+            //     return false;
+            
+            if(s1[i]!=t1[i])
+                return false;
+        }
+        return true;
+        
+    }
+}
