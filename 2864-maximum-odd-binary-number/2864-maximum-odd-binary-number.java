113551524bde05d0654b238a52397f262f19e2e5
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-03-01 20:28:04 +0530

    Time: 2 ms (83.47%), Space: 42.8 MB (98.64%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -3,17 +3,21 @@ class Solution {
         int len=s.length();
         char[] charA=s.toCharArray();
         int count=0;
+        // count all the 1's in the string.
         for(int i=0;i<len;i++)
         {
             if(charA[i]=='1') count++;
         }
         StringBuilder result=new StringBuilder();
+        // Append all the 1's to the start of the string except 1
         for(int i=0;i<count-1;i++)
             result.append("1");
+        // Append all the 0's after the 1's.
         for(int j=0;j<len-count;j++)
         {
             result.append("0");
         }
+        // Append last 1's to the end of the string to make it odd number
         result.append("1");
         
         return result.toString();
