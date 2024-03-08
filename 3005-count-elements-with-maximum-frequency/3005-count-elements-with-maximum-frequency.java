aac11a36cf575db0744c18943c732bd0908dba10
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-03-08 10:28:44 +0530

    Time: 2 ms (70.74%), Space: 42.1 MB (79.05%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,13 +1,15 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
+        //Take a count array that will count the number of occurance of each number of nums array
         int[] count=new int[102];
         for(int i=0;i<nums.length;i++)
         {
             count[nums[i]+1]++;
         }
-        Arrays.sort(count);
-        int max=count[101];
+        Arrays.sort(count); //sort the count array
+        int max=count[101]; // take the max element of count array
         int result=0;
+        //check for the number of element of count array that is equal to max element and add to the result
         for(int i=1;i<=101;i++)
         {
             if(count[i]==max)
