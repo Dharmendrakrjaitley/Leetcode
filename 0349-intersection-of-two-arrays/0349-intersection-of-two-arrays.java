67074d902303376e228a31d427d2c49c70b64e56
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-03-10 12:23:49 +0530

    Time: 3 ms (53.96%), Space: 42.9 MB (89.08%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -2,33 +2,58 @@ import java.util.*;
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        //first approach
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // Set<Integer> set=new HashSet<>();
+        // int i=0;
+        // int j=0;
+        // int len1=nums1.length;
+        // int len2=nums2.length;
+        // while(i<len1 && j<len2)
+        // {
+        //     if(nums1[i]==nums2[j])
+        //     {
+        //         set.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums1[i]<nums2[j])
+        //     {
+        //         i++;
+        //     }
+        //     else 
+        //     {
+        //         j++;
+        //     }
+        // }
+        // int[] result=new int[set.size()];
+        // int x=0;
+        // for(int element:set)
+        // {
+        //     result[x++]=element;
+        // }
+        // return result;
+        
+        //2nd approach
+        
         Set<Integer> set=new HashSet<>();
-        int i=0;
-        int j=0;
-        int len1=nums1.length;
-        int len2=nums2.length;
-        while(i<len1 && j<len2)
+        Set<Integer> intersect=new HashSet<>();
+        for(int i:nums1)
         {
-            if(nums1[i]==nums2[j])
+            set.add(i);
+        }
+        for(int j:nums2)
+        {
+            if(set.contains(j))
             {
-                set.add(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j])
-            {
-                i++;
-            }
-            else 
-            {
-                j++;
+                intersect.add(j);
             }
         }
-        int[] result=new int[set.size()];
+        
+        int[] result=new int[intersect.size()];
         int x=0;
-        for(int element:set)
+        for(int element:intersect)
         {
             result[x++]=element;
         }
