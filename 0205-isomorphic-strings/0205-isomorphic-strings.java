27cb43db4d6cddb056b88c443d1f8b02d37b796c
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-04-02 21:05:59 +0530

    Time: 7 ms (89.77%), Space: 42.5 MB (26.81%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        // Create arrays to store the index of characters in both strings
+        int[] indexS = new int[200]; // Stores index of characters in string s
+        int[] indexT = new int[200]; // Stores index of characters in string t
+        
+        // Get the length of both strings
+        int len = s.length();
+        
+        // If the lengths of the two strings are different, they can't be isomorphic
+        if(len != t.length()) {
+            return false;
+        }
+        
+        // Iterate through each character of the strings
+        for(int i = 0; i < len; i++) {
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
+                return false; // If different, strings are not isomorphic
+            }
+            
+            // Update the indices of characters in both strings
+            indexS[s.charAt(i)] = i + 1; 
+            indexT[t.charAt(i)] = i + 1; 
+        }
+        return true;
+    }
+}
