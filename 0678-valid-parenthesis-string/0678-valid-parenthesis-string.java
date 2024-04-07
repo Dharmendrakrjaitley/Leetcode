6476c3627cada3e3153d349d0284e2e2e7d933e4
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-04-07 21:48:42 +0530

    Time: 0 ms (100.00%), Space: 41 MB (84.07%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkValidString(String s) {
+         int leftMin = 0 , leftMax = 0;
+
+        for(int i =0 ;i< s.length() ; i++){
+            if(s.charAt(i) == '(') {
+                leftMin++ ; leftMax++;
+            }else if(s.charAt(i) == ')'){
+                leftMin-- ; leftMax--;
+            }else{
+                leftMin-- ; leftMax++;
+            }
+            if(leftMax < 0) return false;
+            if(leftMin < 0) leftMin =0;
+        }
+        return leftMin == 0 ? true : false;
+    }
+}
