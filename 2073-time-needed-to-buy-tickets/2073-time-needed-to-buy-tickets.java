282906dfdbae52aee0c13096bfbc0156643a09b2
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2024-04-09 21:46:21 +0530

    Time: 2 ms (70.45%), Space: 41.3 MB (47.55%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -2,7 +2,6 @@ class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int target=tickets[k];
         int result=0;
-        System.out.println("target:" + target);
         while(target>0)
         {
             for(int i=0;i<tickets.length;i++)
@@ -11,7 +10,6 @@ class Solution {
                 {
                     tickets[i]=tickets[i]-1;
                     result++;
-                    System.out.println("result:" + result);
                 }
             }
             target--;
